455c68a882aedf0e87e98770638c896efe813663
Jonathan Hawley-Peters <8185120+jonathanhawleypeters@users.noreply.github.com>
2023-12-22 13:38:54 -0800

    got layout working
    the vite config file is promising too for image processing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<div class="header">
+  <a href="/">
+    <h1>Recipes Only</h1>
+  </a>
+  
+  <div>
+    <style>
+      input {
+        position: relative;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+    </style>
+    <input type="text" placeholder="search" />
+  </div>
+</div>
+
+<slot></slot>
+
+<style>
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
